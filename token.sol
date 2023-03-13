c04621d8b0d160c2b92a3829764034424c6cca4b
Kushaalrajiv <kushaalrajiv@gmail.com>
2023-03-13 12:33:16 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts@4.8.2/token/ERC20/ERC20.sol";
 
 contract Module is ERC20 {
     constructor() ERC20("Module3", "MD") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 }
